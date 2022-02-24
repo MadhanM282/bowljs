abf1ee1c3b2f24c0f8c71a0b94fa6928b5eb5323
MadhanM282 <madhanm282@gmail.com>
2022-02-24 14:44:43 +0530

    ..

---
+++ README.md
@@ -0,0 +1,8 @@
+# the-bowl-company
+
+This is a project which is done during masai construct week and with the contribution of 6 people we completed this project
+
+
+# https://the-bowl-company.vercel.app/
+
+you can click this link to see the project in your machine
---
+++ components/navbar.js
@@ -0,0 +1,20 @@
+function navbar(){
+    return `    <div>
+    <img src="https://www.thebowlcompany.in/wp-content/uploads/2021/09/The-Bowl-Company-Logo.png" alt="">
+</div>
+<div>
+  
+        <ul>
+        <a href="index.html"><li>HOME</li></a>
+        <a href="search.html"><li>SEARCH</li></a>
+          <a href="aboutus.html"><li>ABOUT US</li></a>
+           <a href="ourmenu.html"><li>OUR MENU</li></a>
+         <a href="campaigns.html"><li>CAMPAIGNS</li></a>
+          <a href="#"><li>LOGIN</li></a>
+           <a href="#"><li>CART</li></a>
+        </ul>
+    
+</div>`
+}
+
+export default navbar
